94786df30685df9d5c510cf9bc6ecee324da4e98
balar <balar@192.168.1.43>
2023-03-15 20:21:43 +0530

    TestScript1,2,3 commite and push

---
+++ src/test/java/testScriptsPractice/TestScript3.java
@@ -0,0 +1,25 @@
+package testScriptsPractice;
+
+import org.testng.annotations.Test;
+
+public class TestScript3 
+{
+	@Test
+	public void Run1()
+	{
+		System.out.println("Running");
+	}
+	
+	@Test
+	public void Run2()
+	{
+		System.out.println("Walking");
+	}
+	
+	@Test
+	public void Run3()
+	{
+		System.out.println("Jagging");
+	}
+
+}

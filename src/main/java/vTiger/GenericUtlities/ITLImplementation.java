dafdd24fe0f48783e5cf3d631cd09e83c5cd0737
KrishnaM <balaramkam@gmail.com>
2023-06-20 20:00:30 +0530

    Pushed on 20-06-2023

--- src/main/java/vTiger/GenericUtlities/ITLImplementation.java
+++ src/main/java/vTiger/GenericUtlities/ITLImplementation.java
@@ -1,5 +1,57 @@
 package vTiger.GenericUtlities;
 
-public class ITLImplementation {
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ITLImplementation implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+			String methodName = result.getMethod().getMethodName();
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestSuccess(result);
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestFailure(result);
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestSkipped(result);
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
+	}
+
+	@Override
+	public void onTestFailedWithTimeout(ITestResult result) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onTestFailedWithTimeout(result);
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onStart(context);
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		ITestListener.super.onFinish(context);
+	}
+	
 
 }
